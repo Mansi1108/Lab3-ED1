36c1cd390f0a79e70ba0073d0d33d70d94887f18
JUAN SEBASTIAN SANCHEZ ROJAS <sanchez.iag2@gmail.com>
2020-02-28 13:36:08 -0600

    Se cambio el estilo del proyecto

--- Lab2-ED1/App_Start/BundleConfig.cs
+++ Lab2-ED1/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Lab2_ED1
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/flaty.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
