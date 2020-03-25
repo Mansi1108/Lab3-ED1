9a704163439fdc23a220310c1df94b5516424595
JUAN SEBASTIAN SANCHEZ ROJAS <sanchez.iag2@gmail.com>
2020-03-25 15:36:24 -0600

    Se agrego la funcionalidad de arbol avl

--- CustomLibrary/Estructuras/Arbol.cs
+++ CustomLibrary/Estructuras/Arbol.cs
@@ -48,7 +48,18 @@ namespace CustomLibrary.Estructuras
             InOrder(Raiz);
             return listaOrdenada;
         }
-
+        public List<T> ObtenerListaPost()
+        {
+            listaOrdenada.Clear();
+            PostOrder(Raiz);
+            return listaOrdenada;
+        }
+        public List<T> ObtenerListaPre()
+        {
+            listaOrdenada.Clear();
+            PreOrder(Raiz);
+            return listaOrdenada;
+        }
         // tres casos para eliminacion
         protected override void Delete(Nodo<T> nodo)
         {
@@ -74,6 +85,7 @@ namespace CustomLibrary.Estructuras
                 }
                 nodo.Valor = temp.Valor;
             }
+            BalanceoPost(Raiz);
         }
         // Metodo ayuda para el caso 3 de eliminacion
         private Nodo<T> Derecha(Nodo<T> nodo)
@@ -143,10 +155,12 @@ namespace CustomLibrary.Estructuras
                 else if (value.CompareTo(nodo.Valor) == -1)
                 {
                     nodo.Izquierdo = Insert(nodo.Izquierdo, value);
+                    nodo = Balancear(nodo);
                 }
                 else if (value.CompareTo(nodo.Valor) == 1)
                 {
                     nodo.Derecho = Insert(nodo.Derecho, value);
+                    nodo = Balancear(nodo);
                 }
                 return nodo;
             }
@@ -166,5 +180,135 @@ namespace CustomLibrary.Estructuras
                 InOrder(nodo.Derecho);
             }
         }
+        private void PostOrder(Nodo<T> nodo)
+        {
+            if (nodo.Valor != null)
+            {
+                PostOrder(nodo.Izquierdo);
+                PostOrder(nodo.Derecho);
+                listaOrdenada.Add(nodo.Valor);
+            }
+        }
+        private void PreOrder(Nodo<T> nodo)
+        {
+            if (nodo.Valor != null)
+            {
+                listaOrdenada.Add(nodo.Valor);
+                PreOrder(nodo.Izquierdo);
+                PreOrder(nodo.Derecho);
+            }
+        }
+        private void BalanceoPost(Nodo<T> nodo)
+        {
+            if (nodo.Valor != null)
+            {
+                BalanceoPost(nodo.Izquierdo);
+                BalanceoPost(nodo.Derecho);
+                Balancear(nodo);
+            }
+        }
+
+
+        private int getHeight(Nodo<T> node)
+        {
+            if (node == null) return -1;
+            var IzquierdoH = getHeight(node.Izquierdo);
+            var rightH = getHeight(node.Derecho);
+            return Math.Max(IzquierdoH, rightH) + 1;
+        }
+
+        private int FactorEquilibrio(Nodo<T> nodoActual)
+        {
+            int iz = getHeight(nodoActual.Izquierdo);
+            int der = getHeight(nodoActual.Derecho);
+            int FactorE = iz - der;
+            return FactorE;
+        }
+
+        private Nodo<T> Balancear(Nodo<T> nodoActual)
+        {
+            int factorE = FactorEquilibrio(nodoActual);
+            if (factorE > 1)
+            {
+                if (FactorEquilibrio(nodoActual.Izquierdo) > 0)
+                {
+                    nodoActual = RotacionDer(nodoActual);
+                }
+                else
+                {
+                    nodoActual = RotacionDobDer(nodoActual);
+                }
+            }
+            else if (factorE < -1)
+            {
+                if (FactorEquilibrio(nodoActual.Derecho) > 0)
+                {
+                    nodoActual = RotacionDobIzq(nodoActual);
+                }
+                else
+                {
+                    nodoActual = RotacionIzq(nodoActual);
+                }
+            }
+            return nodoActual;
+        }
+
+        private Nodo<T> RotacionIzq(Nodo<T> nodoActual)
+        {
+            var temp = new Nodo<T>
+            {
+                Valor = nodoActual.Derecho.Valor,
+                Izquierdo = nodoActual.Derecho.Izquierdo,
+                Derecho = nodoActual.Derecho.Derecho
+            };
+            nodoActual.Derecho = temp.Izquierdo;
+            temp.Izquierdo = nodoActual;
+
+            if (nodoActual.Valor.CompareTo(Raiz.Valor) == 0)
+            {
+                Raiz = temp;
+            }
+            return temp;
+        }
+        private Nodo<T> RotacionDer(Nodo<T> nodoActual)
+        {
+            var temp = new Nodo<T>
+            {
+                Valor = nodoActual.Izquierdo.Valor,
+                Izquierdo = nodoActual.Izquierdo.Izquierdo,
+                Derecho = nodoActual.Izquierdo.Derecho
+            };
+            nodoActual.Izquierdo = temp.Derecho;
+            temp.Derecho = nodoActual;
+
+            if (nodoActual.Valor.CompareTo(Raiz.Valor) == 0)
+            {
+                Raiz = temp;
+            }
+            return temp;
+        }
+        private Nodo<T> RotacionDobDer(Nodo<T> nodoActual)
+        {
+            var temp = new Nodo<T>
+            {
+                Valor = nodoActual.Izquierdo.Valor,
+                Izquierdo = nodoActual.Izquierdo.Izquierdo,
+                Derecho = nodoActual.Izquierdo.Derecho
+            };
+            nodoActual.Izquierdo = RotacionIzq(temp);
+            return RotacionDer(nodoActual);
+        }
+        private Nodo<T> RotacionDobIzq(Nodo<T> nodoActual)
+        {
+            var temp = new Nodo<T>
+            {
+                Valor = nodoActual.Derecho.Valor,
+                Izquierdo = nodoActual.Derecho.Izquierdo,
+                Derecho = nodoActual.Derecho.Derecho
+            };
+            nodoActual.Derecho = RotacionDer(temp);
+            return RotacionIzq(nodoActual);
+        }
+
     }
 }
